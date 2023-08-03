eb6aa0357d5cafa7a4e52b1c861d627103e93748
Brady Barker <brady.d.barker@gmail.com>
2023-08-03 18:03:30 -0400

    Created Airport and Flight model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Airport.destroy_all
+# ['LAX', 'ATL', 'ORD', 'JFK', 'TPA', 'PHX', 'SEA'].each { |code| Airport.create(airport_code: code) }
