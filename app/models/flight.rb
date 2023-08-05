5916fcc749cdb0a9fea68773978c99685fce28e3
Brady Barker <brady.d.barker@gmail.com>
2023-08-05 19:03:30 -0400

    Submitted form returns all available flights Available flights are put into seperate form which creates radio buttons This form doesn't yet go to the correct route

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,12 +3,13 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: 'Airport'
 
   scope :distinct_dates, -> { distinct.select(:date).order(:date).distinct }
+  scope :available_flights, ->(flight) { where(departure_airport: flight[:departure_airport_id], arrival_airport: flight[:arrival_airport_id], date: flight[:date]) }
 
   def flight_date_formatted
     date.strftime('%m/%d/%Y')
   end
 
-  def self.available_flights(flight)
-    Flight.where(departure_airport: flight[:departure_airport_id], arrival_airport: flight[:arrival_airport_id], date: flight[:date])
+  def flight_radio_formatted
+    "#{departure_airport.airport_code} -> #{arrival_airport.airport_code} At #{time.strftime('%H:%M')}"
   end
 end
