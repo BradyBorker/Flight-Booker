3ba1356bc4636b75a75897360345ce15728f0a45
Brady Barker <brady.d.barker@gmail.com>
2023-08-06 17:48:45 -0400

    Created two fields_for forms within Booking#new view. These fields are for Flight and Passengers

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,7 @@
+class BookingsController < ApplicationController
+    def new
+        @booking = Booking.new
+        @flight = Flight.find(params[:flight_selected])
+        @passengers = params[:passenger_count].to_i.times.map { Passenger.new }
+    end
+end
