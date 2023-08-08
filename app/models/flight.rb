c1e82369b42057e7000d7f3b480384a0d9c3a042
Brady Barker <brady.d.barker@gmail.com>
2023-08-08 18:42:25 -0400

    Added basic validations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,7 +2,7 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
 
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   scope :distinct_dates, -> { distinct.select(:date).order(:date).distinct }
   scope :available_flights, ->(flight) { where(departure_airport: flight[:departure_airport_id], arrival_airport: flight[:arrival_airport_id], date: flight[:date]) }
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,5 @@
 class Passenger < ApplicationRecord
+    validates :name, :email, presence: true
+
     has_and_belongs_to_many :bookings
 end
